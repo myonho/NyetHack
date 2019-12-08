4a5bbddac1e467f5b8495748df23e1c5a9c2a337
myonho <k.mihob1028@gmail.com>
2019-12-09 00:06:27 +0900

    try repl

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -23,3 +23,27 @@ fun main(args: Array<String>) {
     }
     println(name + healthStatus)
 }
+// Kotlinのちょっとしたコード試すのにKotlin REPLが便利
+// 1 in 1..3
+//res0: kotlin.Boolean = true
+//
+//1 in 3 downTo 1
+//res1: kotlin.Boolean = true
+//
+//(1..3).toList()
+//res2: kotlin.collections.List<kotlin.Int> = [1, 2, 3]
+//
+//1 in 1 until 3
+//res3: kotlin.Boolean = true
+//
+//3 in 1 until 3
+//res4: kotlin.Boolean = false
+//
+//2 in 1..3
+//res5: kotlin.Boolean = true
+//
+//2 !in 1..3
+//res6: kotlin.Boolean = false
+//
+//'x' in 'a'..'z'
+//res7: kotlin.Boolean = true
