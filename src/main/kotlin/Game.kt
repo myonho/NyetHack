12da8e1d49f591289a911342d7dc61a7ac435872
myonho <k.mihob1028@gmail.com>
2019-12-09 00:02:12 +0900

    リファクタリング

--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -4,15 +4,22 @@
  * === 2つのインスタンスが同じ参照である（!==）
  *
  * + ：文字列結合（string concatenation）
+ *
+ * && || ! ：論理演算子（logical operators）
+ * && ：論理積演算子（logical 'and' operator）左辺*右辺...どちらも真で真
+ * || ：論理和演算子（logical 'or' operator）左辺+右辺...どちらかが真で真
  * */
 
 fun main(args: Array<String>) {
     val name = "Madrigal"
     var healthPoints = 100
 
-    if (healthPoints == 100) {
-        println("$name is in excellent condition!!")
-    } else {
-        println("$name is in awful condition!")
+    val healthStatus = when (healthPoints) {
+        100 -> " is in excellent condition!!"
+        in 90..99 -> " has a few scratches"
+        in 75..89 -> " has some mirror wounds"
+        in 15..74 -> " look pretty hurt"
+        else -> " is in awful condition!"
     }
+    println(name + healthStatus)
 }
